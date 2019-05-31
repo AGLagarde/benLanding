324486f3e2d0a685c3f7aba71624feefa015eefc
LAGARDE Anne <a.lagarde@marazinedigital.com>
2019-05-31 15:19:02 +0200

    new maquette

---
+++ src/components/Curved.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="curved">
+        <div id="more" class="curved__block">
+            <h2 class="h2">Commencez dès maintenant
+                à vous organiser </h2>
+            <div class="curved__illu">
+                <picture>
+                    <img class="homepage" src="../assets/img/home_01.jpg" alt="homepage">
+                    <img class="list" src="../assets/img/list_02.jpg" alt="list page">
+                    <img class="notif" src="../assets/img/notif_03.jpg" alt="notification page">
+                </picture>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <ul>
+      <li class="footer_link"><a href="#">© 2019 Ben Colocation</a></li>
+      <li class="footer_link"><a href="#">Termes et conditions</a></li>
+      <li class="footer_link"><a :href="`//${url.instagram}`">Instagram</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                url: {
+                    instagram: 'www.instagram.com'
+                }
+
+            }
+        }
+    }
+</script>
+
+
